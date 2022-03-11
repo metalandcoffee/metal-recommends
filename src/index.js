3edaeace4f4bea3d00bc95fd081e00b829590c19
Ebonie Butler <ebonie@yikesinc.com>
2022-03-10 20:18:50 -0500

    last.fm api fetch

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const SearchBar = ({ onChange }) => {
+const SearchBar = ({ onSubmit }) => {
   // Track current search term locally until form is submitted.
   const [localTerm, setLocalTerm] = useState('');
   const handleChange = (e) => {
@@ -10,7 +10,7 @@ const SearchBar = ({ onChange }) => {
   };
   const handleSubmit = (e) => {
     e.preventDefault();
-    onChange(localTerm);
+    onSubmit(localTerm);
   };
   return (
     <form onSubmit={handleSubmit}>
@@ -19,9 +19,34 @@ const SearchBar = ({ onChange }) => {
   );
 };
 
+const Results = ({ term }) => {
+  const [artists, setArtists] = useState('Type in an artist to receive recommendations✨');
+  useEffect(() => {
+    if (!term) {
+      return;
+    }
+    console.log(term);
+    fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=${term.toLowerCase()}&api_key=${process.env.REACT_APP_LAST_FM_API_KEY}&format=json&limit=20`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setArtists(data.similarartists.artist);
+      });
+  }, [term]);
+  return (
+    <div id="results">{Array.isArray(artists) ? artists.map( artist => {
+      return (
+        <>
+          <p>{ artist.name }</p>
+          <img src={artist.image[1]['#text']} alt="" />
+        </>
+      )
+    }) : artists }</div>
+  )
+}
+
 const App = () => {
   const [term, setTerm] = useState('');
-  console.log(term);
   const updateSearchTerm = (term) => {
     setTerm(term);
   };
@@ -30,7 +55,8 @@ const App = () => {
       <header className="App-header">
         <h1>Metal Recommends...</h1>
       </header>
-      <SearchBar term={term} onChange={updateSearchTerm} />
+      <SearchBar onSubmit={updateSearchTerm} />
+      <Results term={term} />
     </div>
   );
 };
