c3a375c15ad83d36bb8a2e8fa2e542ab801e2750
Ebonie Butler <ebonie@yikesinc.com>
2022-03-11 11:53:01 -0500

    trying to get album artwork

--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// Helper function to get top album artwork for queried artists.
+async function setAlbumArtworks(artists) {
+  const artistsTopAlbum = await artists.map( async (artist) => {
+    const topAlbum = await fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=${artist.name}&api_key=${process.env.REACT_APP_LAST_FM_API_KEY}&format=json&limit=1`)
+    .then(response => response.json())
+    .then(data => {
+      return 'hi';
+    })
+  });
+  console.log(artistsTopAlbum);
+  return artistsTopAlbum;
+}
+
+/**
+ * Search Bar Component.
+ */
 const SearchBar = ({ onSubmit }) => {
   // Track current search term locally until form is submitted.
   const [localTerm, setLocalTerm] = useState('');
@@ -19,6 +35,9 @@ const SearchBar = ({ onSubmit }) => {
   );
 };
 
+/**
+ * Display Results Component.
+ */
 const Results = ({ term }) => {
   const [artists, setArtists] = useState('Type in an artist to receive recommendations✨');
   useEffect(() => {
@@ -26,25 +45,30 @@ const Results = ({ term }) => {
       return;
     }
     console.log(term);
-    fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=${term.toLowerCase()}&api_key=${process.env.REACT_APP_LAST_FM_API_KEY}&format=json&limit=20`)
+    fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=${term.toLowerCase()}&api_key=${process.env.REACT_APP_LAST_FM_API_KEY}&format=json&limit=5`)
       .then(response => response.json())
       .then(data => {
         console.log(data);
-        setArtists(data.similarartists.artist);
+        const formattedArtists = async () => {
+          const artists = await setAlbumArtworks(data.similarartists.artist);
+          setArtists(artists);
+        }
+        formattedArtists().catch(console.error);
       });
   }, [term]);
+
   return (
-    <div id="results">{Array.isArray(artists) ? artists.map( artist => {
+    <div id="results">{Array.isArray(artists) ? artists.map(artist => {
       return (
-        <>
-          <p>{ artist.name }</p>
-          <img src={artist.image[1]['#text']} alt="" />
-        </>
+        <p key={artist.name}>{artist.name}</p>
       )
-    }) : artists }</div>
+    }) : artists}</div>
   )
 }
 
+/**
+ * App Entrypoint Component.
+ */
 const App = () => {
   const [term, setTerm] = useState('');
   const updateSearchTerm = (term) => {
