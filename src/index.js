8933aa69b5d2964d8f926c9d7e3b7fce1f6da197
Ebonie Butler <ebonie@yikesinc.com>
2022-03-11 21:00:02 -0500

    general refactoring

--- src/index.js
+++ src/index.js
@@ -13,21 +13,6 @@ import './index.css';
  * Lil Nas
  */
 
-// Helper function to get top album artwork for queried artists.
-// @link https://stackoverflow.com/questions/65655885/why-does-using-async-await-in-map-function-still-return-promises-and-not-the-res
-async function setAlbumArtworks(artists) {
-  let artistsTopAlbum = artists.map(async (artist) => {
-    const data = await fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=${artist.name}&api_key=${process.env.REACT_APP_LAST_FM_API_KEY}&format=json&limit=1`)
-      .then(response => response.json());
-    return {
-      name: artist.name,
-      albumTitle: undefined !== data.topalbums && 0 < data.topalbums.album.length ? data.topalbums.album[0].name : 'N/A',
-      image: undefined !== data.topalbums && 0 < data.topalbums.album.length ? data.topalbums.album[0].image[3]['#text'] : 'https://via.placeholder.com/300'
-    };
-  });
-  return await Promise.all(artistsTopAlbum);
-}
-
 /**
  * Search Bar Component.
  */
@@ -57,19 +42,50 @@ const Results = ({ term }) => {
     if (!term) {
       return;
     }
-    console.log(term);
-    fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=${term.toLowerCase()}&api_key=${process.env.REACT_APP_LAST_FM_API_KEY}&format=json&limit=5`)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        const formattedArtists = async () => {
-          const artists = await setAlbumArtworks(data.similarartists.artist);
-          setArtists(artists);
-        }
-        formattedArtists().catch(console.error);
-      });
-  }, [term]);
 
+    const getArtists = async () => {
+      const response = await fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=${term.toLowerCase()}&api_key=${process.env.REACT_APP_LAST_FM_API_KEY}&format=json&limit=5`);
+      const resultsPromise = response.json();
+      return resultsPromise;
+    }
+    const getAlbums = async (artist) => {
+      const response = await fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=${artist.name}&api_key=${process.env.REACT_APP_LAST_FM_API_KEY}&format=json&limit=1`);
+      const resultsPromise = response.json();
+      return resultsPromise;
+    }
+    // @link https://stackoverflow.com/questions/65655885/why-does-using-async-await-in-map-function-still-return-promises-and-not-the-res
+    const setAlbumForArtists = async (results) => {
+      const artists = results.similarartists.artist;
+      const albums = await Promise.all(
+        artists.map(async artist => {
+          const album = await getAlbums(artist);
+          return album;
+        })
+      );
+      console.log(albums);
+      // let artistsTopAlbum = artists.map(async (artist) => {
+      //   const data = await fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=${artist.name}&api_key=${process.env.REACT_APP_LAST_FM_API_KEY}&format=json&limit=1`)
+      //     .then(response => response.json());
+      //   return {
+      //     name: artist.name,
+      //     albumTitle: undefined !== data.topalbums && 0 < data.topalbums.album.length ? data.topalbums.album[0].name : 'N/A',
+      //     image: undefined !== data.topalbums && 0 < data.topalbums.album.length ? data.topalbums.album[0].image[3]['#text'] : 'https://via.placeholder.com/300'
+      //   };
+      //});
+      //return await Promise.all(artistsTopAlbum);
+    }
+
+    console.log(term);
+    const fetchData = async () => {
+      const results = await getArtists();
+      setAlbumForArtists(results);
+      
+      //setArtists(await getArtists());
+    }
+
+    fetchData().catch(console.error);
+  }, [term]);
+  console.log(artists);
   return (
     <ul id="results">{Array.isArray(artists) ? artists.map(artist => {
       return (
