94c7cba5f5f83cd6be69c68cdc7839088f2f8bf3
Ebonie Butler <ebonie@metalandcoffee.com>
2022-03-12 20:40:12 -0500

    more code cleanup

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 /**
- * 
+ *
  * Test Cases:
  * Tora
  * Doja Cat
  * Cult of Luna
  * Opeth
  * Parcels
- * Lil Nas
+ * Lil Nas X
  */
 
 /**
@@ -37,68 +37,70 @@ const SearchBar = ({ onSubmit }) => {
  * Display Results Component.
  */
 const Results = ({ term }) => {
-  const [artists, setArtists] = useState('Type in an artist to receive recommendations✨');
+  const [artists, setArtists] = useState([]);
+
   useEffect(() => {
     if (!term) {
       return;
     }
 
     const getArtists = async () => {
-      const response = await fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=${term.toLowerCase()}&api_key=${process.env.REACT_APP_LAST_FM_API_KEY}&format=json&limit=5`);
+      const response = await fetch(
+        `http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=${term.toLowerCase()}&api_key=${
+          process.env.REACT_APP_LAST_FM_API_KEY
+        }&format=json&limit=9`
+      );
       const resultsPromise = response.json();
       return resultsPromise;
-    }
-    const getAlbums = async (artist) => {
-      const response = await fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=${artist.name}&api_key=${process.env.REACT_APP_LAST_FM_API_KEY}&format=json&limit=1`);
+    };
+    const getAlbum = async (artist) => {
+      const response = await fetch(
+        `http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=${artist.name}&api_key=${process.env.REACT_APP_LAST_FM_API_KEY}&format=json&limit=1`
+      );
       const resultsPromise = response.json();
       return resultsPromise;
-    }
-    // @link https://stackoverflow.com/questions/65655885/why-does-using-async-await-in-map-function-still-return-promises-and-not-the-res
-    const setAlbumForArtists = async (results) => {
+    };
+
+    const fetchAndSetResults = async () => {
+      const results = await getArtists();
       const artists = results.similarartists.artist;
-      const albums = await Promise.all(
-        artists.map(async artist => {
-          const album = await getAlbums(artist);
-          return album;
+      const artistsTopAlbum = await Promise.all(
+        artists.map(async (artist) => {
+          const album = await getAlbum(artist);
+          return {
+            name: artist.name,
+            albumTitle:
+              0 < album?.topalbums?.album?.length
+                ? album.topalbums.album[0].name
+                : 'N/A',
+            image:
+            album?.topalbums?.album?.[0] && '' !== album.topalbums.album[0].image[3]['#text']
+                ? album.topalbums.album[0].image[3]['#text']
+                : 'https://via.placeholder.com/300',
+          };
         })
       );
-      console.log(albums);
-      // let artistsTopAlbum = artists.map(async (artist) => {
-      //   const data = await fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=${artist.name}&api_key=${process.env.REACT_APP_LAST_FM_API_KEY}&format=json&limit=1`)
-      //     .then(response => response.json());
-      //   return {
-      //     name: artist.name,
-      //     albumTitle: undefined !== data.topalbums && 0 < data.topalbums.album.length ? data.topalbums.album[0].name : 'N/A',
-      //     image: undefined !== data.topalbums && 0 < data.topalbums.album.length ? data.topalbums.album[0].image[3]['#text'] : 'https://via.placeholder.com/300'
-      //   };
-      //});
-      //return await Promise.all(artistsTopAlbum);
-    }
+      setArtists(artistsTopAlbum);
+    };
 
-    console.log(term);
-    const fetchData = async () => {
-      const results = await getArtists();
-      setAlbumForArtists(results);
-      
-      //setArtists(await getArtists());
-    }
-
-    fetchData().catch(console.error);
+    fetchAndSetResults().catch(console.error);
   }, [term]);
-  console.log(artists);
   return (
-    <ul id="results">{Array.isArray(artists) ? artists.map(artist => {
-      return (
-        <li key={artist.name}>
-          <img src={artist.image} alt={artist.albumTitle} />
-          <p>{artist.name}</p>
-          <p>{artist.albumTitle}</p>
-        </li>
-
-      )
-    }) : artists}</ul>
-  )
-}
+    <ul id="results">
+      {0 < artists.length
+        ? artists.map((artist) => {
+            return (
+              <li key={artist.name}>
+                <img src={artist.image} alt={artist.albumTitle} />
+                <p>{artist.name}</p>
+                <p>{artist.albumTitle}</p>
+              </li>
+            );
+          })
+        : 'Type in an artist to receive album recommendations from similar artists✨'}
+    </ul>
+  );
+};
 
 /**
  * App Entrypoint Component.
